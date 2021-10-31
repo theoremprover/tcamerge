122b86087ff511e606e7e950c28bc2a15e1d1d10
theoremprover <none@none.com>
2021-10-31 23:13:56 +0100

    dfs

--- tcamerge/src/Main.hs
+++ tcamerge/src/Main.hs
@@ -1,6 +1,17 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- stack exec DtdToHaskell -- tca.dtd >src\Extsubset.hs
+
 module Main where
+
+--import Text.XML.HaXml
+import Text.XML.HaXml.XmlContent
+
+import Extsubset
+
+xmlFileName = "Branch.tca"
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
-
+    a :: Xmi'XMI <- fReadXml xmlFileName
+    print a
