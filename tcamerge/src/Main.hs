df6bd172b939a33d5d1074f00d76aa8c7b4744ef
theoremprover <none@none.com>
2021-11-01 18:49:27 +0100

    dfs

--- tcamerge/src/Main.hs
+++ tcamerge/src/Main.hs
@@ -27,15 +27,21 @@ main :: IO ()
 main = do
 	(base,embedbase) <- readElementTree "Branch.tca"
 	(mod1,embedmod1) <- readElementTree "newtool1.tca"
-	let [mergedelement] = mergeList sameElement mergeElements [base] [mod1]
+	let [mergedelement] = mergeLists sameElement mergeElements [base] [mod1]
 	writeFile ("merged.tca") $ render $ Text.XML.HaXml.Pretty.document $ embedmod1 mergedelement
 
+sameAttribute (qname1,_) (qname2,_) = qname1==qname2
+mergeAttributes (qname1,AttValue esrs1) (qname2,AttValue esrs2) | qname1==qname2 = (qname1,AttValue (esrs1++esrs2))
+
 sameElement (Elem qname1 _ _) (Elem qname2 _ _) = qname1==qname2
-
 mergeElements (Elem qname1 attrs1 contents1) (Elem qname2 attrs2 contents2) | qname1==qname2 =
-	Elem qname1 (mergeList sameAttribute mergeAttributes attrs1 attrs2) (mergeList sameContent mergeContents contents1 contents2)
+	Elem qname1 (mergeLists sameAttribute mergeAttributes attrs1 attrs2) (mergeLists sameContent mergeContents contents1 contents2)
 
-mergeList :: (a -> a -> Bool) -> (a -> a -> a) -> [a] -> [a] -> [a]
-mergeList same_elem merge_elem (elem1:rest1) (elem2:rest2) | same_elem elem1 elem2 =
-	merge_elem elem1 elem2 : mergeList rest1 rest2
+sameContent (CElem elem1 _) (CElem elem2 _) = sameElement elem1 elem2
+sameContent cont1 cont2 = cont1==cont2
+mergeContents (CElem elem1 i1) (CElem elem2 i2) = CElem (mergeElements elem1 elem2) i1
 
+mergeLists :: (a -> a -> Bool) -> (a -> a -> a) -> [a] -> [a] -> [a]
+mergeLists same_elem merge_elem (elem1:rest1) (elem2:rest2) | same_elem elem1 elem2 =
+	merge_elem elem1 elem2 : mergeLists same_elem merge_elem rest1 rest2
+mergeLists _ _ _ _ = error "not implemented"
