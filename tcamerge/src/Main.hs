48af00416067f31af4899e98ce5fffd5a3e263de
theoremprover <none@none.com>
2021-11-12 16:37:06 +0100

    dfsdsa

--- tcamerge/src/Main.hs
+++ tcamerge/src/Main.hs
@@ -5,23 +5,28 @@ module Main where
 
 import Text.XML.Light
 import Data.List
+import Control.Monad
+import Data.Maybe
 
-type XML = [Content]
+type XML = Element
 
-readXML :: FilePath -> IO XML
+readXML :: FilePath -> IO Element
 readXML filepath = do
-	content <- readFile filepath
-	return $ parseXML content
+	f <- readFile filepath
+	case parseXMLDoc f of
+		Nothing -> error $ "ERROR parsing " ++ filepath
+		Just element -> return element
 
-writeXML :: FilePath -> [Content] -> IO ()
-writeXML filepath [Elem element] = do
-	writeFile filepath $ ppTopElement element
+writeXML :: FilePath -> XML -> IO ()
+writeXML filepath xml = do
+	writeFile filepath $ ppTopElement xml
 
 main :: IO ()
 main = do
-	xml1 <- readXML "test1.xml"
+	xml1 <- readXML "Branch.tca"
 	xml2 <- readXML "test2.xml"
-	print $ mergeXML xml1 xml2
+	putStrLn $ ppTopElement $ mergeXML xml1 xml2
+--	writeXML "testout.xml" 
 
 mergeXML :: XML -> XML -> XML
-mergeXML xml1 xml2 =
+mergeXML xml1 xml2 = xml1
