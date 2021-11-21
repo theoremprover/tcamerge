7c265d816d88294106db104eb0d3226d73b5f0b9
theoremprover <none@none.com>
2021-11-21 21:06:33 +0100

    dfsdsa

--- tcamerge/src/Main.hs
+++ tcamerge/src/Main.hs
@@ -25,7 +25,8 @@ main :: IO ()
 main = do
 	xml1 <- readXML "newtool1.tca"
 	xml2 <- readXML "newtool2.tca"
-	writeXML "merged.tca" $ merge [xml1] [xml2]
+	let [res_xml] = diff xml1 xml2
+	writeXML "merged.tca" res_xml	
 
 
 ---- Same ----------------------	
@@ -38,38 +39,55 @@ instance Same Element where
 	same (Element name1 attribs1 contents1 line1) (Element name2 attribs2 contents2 _) =
 		name1==name2 && idAttrib attribs1 == idAttrib attribs2
 		where
-		idAttrib attribs = case filter ((=="id").attrKey) attribs of
+		idAttrib attribs = case filter ((=="id").qName.attrKey) attribs of
 			[Attr key val] -> Just val
 			_              -> Nothing
 
-instance Same Attrib where
+instance Same Attr where
 	same (Attr qname1 _) (Attr qname2 _) = qname1==qname2
 
----- Mergable -------------------
+---- Diff Tags ------------------
 
-diffList :: Mergable a => [a] -> [a] -> [a]
-mergeList l1 [] = l1
-mergeList [] l2 = l2
-mergeList (e1:l1s) (e2:l2s) | e1 `same` e2 = merge e1 e2 : mergeList l1s l2s
-mergeList l1@(e1:l1s) l2@(e2:l2s) = case (first_same e1 l2s,first_same e2 l1s) of
-	((inserted,rest),(_,[])) | not (null inserted) -> inserted ++ mergeList l1 rest
-	((_,[]),(deleted,rest))  | not (null deleted)  -> mergeList 
+
+---- Diffable -------------------
+
+class Same a => Diffable a where
+	diff :: a -> a -> [a]
+	mkInserted :: a -> a
+	mkDeleted  :: a -> a
+	isInserted :: a -> Bool
+	isDeleted  :: a -> Bool
+
+{--
+diffList [A,B] [A,C,B] = [ A, Inserted C, B ]
+diffList [A,B,C] [B,C] = [ Deleted A, B ]
+diffList [A] [B] = [ Deleted A, Inserted B ]
+--}
+diffList :: (Diffable a) => [a] -> [a] -> [a]
+diffList l1 [] = l1
+diffList [] l2 = l2
+diffList (e1:l1s) (e2:l2s) | e1 `same` e2 = diff e1 e2 ++ diffList l1s l2s
+diffList l1@(e1:l1s) l2@(e2:l2s) = case (collect_diffs_and_rest e1 l2s,collect_diffs_and_rest e2 l1s) of
+	((inserted,rest),(_,[])) | not (null inserted) -> map mkInserted inserted ++ diffList l1 rest
+	((_,[]),(deleted,rest))  | not (null deleted)  -> map mkDeleted  deleted  ++ diffList l2 rest
+	((_,[]),(_,[]))                                -> mkDeleted e1 : mkInserted e2 : diffList l1s l2s
 	where
-	first_same e l = span (not.(same e)) l
+	collect_diffs_and_rest :: Same a => a -> [a] -> ([a],[a])
+	collect_diffs_and_rest e l = span (not.(same e)) l
 
-class Same a => Mergable a where
-	merge :: a -> a -> [a]
+insertedQName = QName "_INSERTED_" Nothing Nothing
+deletedQName  = QName "_DELETED_"  Nothing Nothing
+instance Diffable Element where
+	diff elem1@(Element name1 attribs1 contents1 line1) elem2@(Element name2 attribs2 contents2 _)
+		| elem1 `same` elem2 = [ Element name1 (diffList attribs1 attribs2) (diffList contents1 contents2) line1 ]
+	diff elem1 elem2 = []
+	mkInserted elem = Element insertedQName [] [Elem elem] Nothing
+	mkDeleted elem  = Element deletedQName  [] [Elem elem] Nothing
+	isInserted = (==insertedQName).elName
+	isDeleted  = (==deletedQName).elName
 
-instance Mergable Element where
-	merge elem1@(Element name1 attribs1 contents1 line1) elem2@(Element name2 attribs2 contents2 _) | elem1 `same` elem2 =
-		Element name1 (mergeList attribs1 attribs2) (mergeList contents1 contents2) line1
-	merge elem1@(Element name1 attribs1 contents1 line1) elem2@(Element name2 attribs2 contents2 _)
-		case name1==name2 of
-			True -> case idAttrib attribs1 == idAttrib elem2 attribs2 of
-				True  -> 
-				False -> 
+instance Diffable Content where
+	diff (Elem elem1) (Elem elem2) = map Elem $ diff elem1 elem2
+	diff (Text (CData CDataText text1 _)) (Text (CData CDataText text2 _)) = map () $ diff text1 text2
 
-instance (Mergable a) => Mergable [a] where
-	merge (a:as) (b:bs) = 
-
-
+instance Diffable Attr where
