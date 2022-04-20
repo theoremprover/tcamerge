67138e1830c070c4fada4ee6b2df8659946f588f
theoremprover <none@none.com>
2022-04-20 22:01:21 +0200

    dfsdsa

--- tcamerge/app/Main.hs
+++ tcamerge/app/Main.hs
@@ -7,3 +7,19 @@ main :: IO ()
 main = do
 	putStrLn "TEST"
 
+--infixl 4 <*>
+
+class Applicative p where
+	pure  :: a -> p a
+	(<*>) :: p (a -> b) -> p a -> p b
+
+class Functor f where
+	fmap :: (a -> b) -> f a -> f b
+
+class Category y where
+	id  :: y a a
+	(.) :: y b c -> y a b -> y a c
+
+class Monad m where
+	return :: a -> m a
+	(>>=)  :: m a -> (a -> m b) -> m b
